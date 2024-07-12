ef561044c619d1191cdb46f5092b5150dea288e4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-07-12 17:42:36 -0400

    Add Iron config and python

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,11 @@
+local utils = require('utils')
+utils.register_iron_keys('print(%s)')
+
+local iron = require('iron.core')
+
+utils.bbind('n', '<localleader>vr', function()
+  local path = vim.fn.fnamemodify(vim.fn.expand('%:r'), ':~:.')
+  local modname, _ = string.gsub(path, '/', '.')
+  local cmd = string.format('import importlib; importlib.reload(%s)', modname)
+  iron.send(nil, {cmd})
+end)
---
+++ lua/plugins/iron.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "hkupty/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+      local pyrepl = {
+        -- `python -m IPython` ensures we're always using the curren venv's IPython
+        command = { "python", "-m", "IPython", "--no-autoindent", "--no-confirm-exit" },
+        format = require("iron.fts.common").bracketed_paste,
+      }
+
+      iron.setup({
+        config = {
+          repl_open_cmd = "botright 15 split",
+          buflisted = true,
+          repl_definition = {
+            python = pyrepl,
+            r = { command = { "R" } },
+            julia = { command = { "julia" } },
+            sh = { command = { "bash" } },
+            ps1 = { command = { "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" } },
+          },
+        },
+      })
+    end,
+  },
+}
