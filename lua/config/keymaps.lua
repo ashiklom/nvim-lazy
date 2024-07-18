da6e1a5f74096435d873f49d98c8e68a90b37e91
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-07-18 13:05:03 -0400

    Add toggle completion key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,34 @@ vim.keymap.set("n", "<leader>wj", "<C-w>j", { silent = true, desc = "Window down
 vim.keymap.set("n", "<leader>wk", "<C-w>k", { silent = true, desc = "Window up" })
 vim.keymap.set("n", "<leader>wl", "<C-w>l", { silent = true, desc = "Window left" })
 vim.keymap.set("n", "<leader>wh", "<C-w>h", { silent = true, desc = "Window right" })
+
+-- Toggle completion (https://github.com/hrsh7th/nvim-cmp/issues/261#issuecomment-930003962)
+vim.g.cmp_toggle_flag = true -- initialize
+local normal_buftype = function()
+  return vim.api.nvim_get_option_value('buftype', {scope = "local"}) ~= 'prompt'
+end
+
+local toggle_completion = function()
+  local ok, cmp = pcall(require, "cmp")
+  if ok then
+    local next_cmp_toggle_flag = not vim.g.cmp_toggle_flag
+    if next_cmp_toggle_flag then
+      print("completion on")
+    else
+      print("completion off")
+    end
+    cmp.setup {
+      enabled = function()
+        vim.g.cmp_toggle_flag = next_cmp_toggle_flag
+        if next_cmp_toggle_flag then
+          return normal_buftype()
+        else
+          return next_cmp_toggle_flag
+        end
+      end
+    }
+  else
+    print("completion not available")
+  end
+end
+vim.keymap.set("n", "<leader>ux", toggle_completion)
